ba1911426544135cf23270d9a5886277ec6384f4
SanikaSavalajkar <114510422+SanikaSavalajkar@users.noreply.github.com>
2023-10-01 00:25:19 +0530

    Update QueueReversal.java

--- Queues/QueueReversal.java
+++ Queues/QueueReversal.java
@@ -39,3 +39,25 @@ public class QueueReversal {
  * Output:
  * 10 9 8 7 6 5 4 3 2 1
  */
+/*This Java program demonstrates the reversal of a queue using a stack. The `reverse` method takes a `Queue<Integer>` as input and reverses its elements using a `Stack<Integer>`. The main components of the program are as follows:
+
+1. **`reverse` method:**
+   - Takes a `Queue<Integer>` (`q`) as input.
+   - Creates a `Stack<Integer>` (`s`) to temporarily store elements.
+   - Empties the original queue, pushing each element onto the stack.
+   - Empties the stack, pushing each element back into the queue.
+   - As a result, the order of elements in the queue is reversed.
+
+2. **`main` method:**
+   - Creates a `Queue<Integer>` (`q`) using a `LinkedList`.
+   - Enqueues elements 1 through 10 into the queue.
+   - Calls the `reverse` method to reverse the order of elements in the queue.
+   - Prints the reversed queue.
+
+3. **Explanation:**
+   - The program uses a stack to reverse the order of elements in the queue.
+   - The `reverse` method iteratively removes elements from the front of the queue and pushes them onto the stack.
+   - After the queue is emptied, the method pops elements from the stack and enqueues them back into the queue, effectively reversing the order.
+   - The output demonstrates that the original queue `{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}` has been reversed to `{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}`.
+
+The reversal is achieved by using the Last-In-First-Out (LIFO) property of the stack data structure to reverse the order of elements in the queue.*/
